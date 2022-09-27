ce90214a5a714c50e7f773c257219dcbdfdb90df
Mateo-tech <mat.hladky@gmail.com>
2022-09-27 19:31:20 +0200

    Added the Article model

--- b_log/articles/models.py
+++ b_log/articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    summary = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    content = models.TextField()
+    meta_desc = models.CharField(max_length=150, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
